ccc50590ce10e9c71e5c4c6150ef0e35437dda67
agrazh <andy.grazh@gmail.com>
2020-12-25 23:47:46 +0100

    Initial commit

---
+++ js/hide_nav_block.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    hide_mkdocs_footer();
+});
+
+
+function hide_mkdocs_footer() {
+    document.querySelectorAll('.md-footer-meta.md-typeset')[0].remove();
+}
+
+
+/* function hide_sidebar_left(){
+    document.querySelectorAll('.md-nav.md-nav--primary')[0].setAttribute("style", "visibility: hidden;")
+    document.querySelectorAll('.md-nav .md-nav--secondary label')[0].remove()
+} */
+
+
+// <nav class="md-nav md-nav--primary" aria-label="Navigation" data-md-level="0">
+// <nav class="md-nav md-nav--secondary" aria-label="Table of contents">
+//<label class="md-nav__title"> ('label.md-nav__title')
